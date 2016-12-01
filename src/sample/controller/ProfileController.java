63b2a58d1ab971d549c2e6a9d8eae53ffa300f0e
ashley <msashleychen@gmail.com>
2016-11-30 23:30:46 -0500

    Created Repo Window

--- src/sample/controller/ProfileController.java
+++ src/sample/controller/ProfileController.java
@@ -3,16 +3,18 @@ package sample.controller;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import org.kohsuke.github.GHMyself;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.PagedIterable;
+import sample.application.Profile;
+import sample.application.Repo;
 
 import java.io.IOException;
 import java.net.URL;
@@ -28,40 +30,48 @@ public class ProfileController  implements Initializable {
 
     private GitHub gitHub;
     private GHMyself you;
-    protected List<String> asianCurrencyList = new ArrayList<>();
-    protected List<String> europeanCurrencyList = new ArrayList<>();
+    private GHRepository pickedRepo;
+    private PagedIterable<GHRepository> repos = null;
+    protected List<String> repoList = new ArrayList<>();
     protected ListProperty<String> listProperty = new SimpleListProperty<>();
 
     public ProfileController() throws IOException {
     }
 
     public void initialize(URL location, ResourceBundle resources) {
-        //Need to connect to GitHub from LoginController. Except how?
-        PagedIterable<GHRepository> repos = null;
         try {
             repos = gitHub.getMyself().listRepositories();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(repos);
-        int count = 0;
-        while(repos.iterator().hasNext() && count < 5){
-            GHRepository repo = repos.iterator().next();
-            System.out.println(repo.getFullName());
-            count++;
-        }
         for(GHRepository repo:repos){
-            asianCurrencyList.add(repo.getFullName());
+            repoList.add(repo.getName());
         }
-        asianCurrencyList.add("GHC");
         myListView.itemsProperty().bind(listProperty);
-        listProperty.set(FXCollections.observableArrayList(asianCurrencyList));
+        listProperty.set(FXCollections.observableArrayList(repoList));
+
+        button.setOnAction((event) -> {
+            try{
+                Repo repo = new Repo();
+                System.out.println(pickedRepo.getFullName() + " In Controller");
+                repo.setupRepo(pickedRepo);
+                repo.display(button);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        });
 
     }
 
     @FXML
     public void getRepo() {
 
+    }
+
+    @FXML public void handleMouseClick(MouseEvent arg0) throws IOException {
+        System.out.println("clicked on " + myListView.getSelectionModel().getSelectedItem());
+        pickedRepo = gitHub.getMyself().getRepository(myListView.getSelectionModel().getSelectedItem().toString());
     }
 
     public void setupProfileController(GitHub _github){
